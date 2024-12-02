68ae488fcaaeb3655a99812d11208b4f4202c077
Sanzentyo <sanzen.tyo@gmail.com>
2024-12-02 22:00:33 +0900

    -1 ルーティングの追加: YouTubeへのリダイレクトを提供する新しいエンドポイントを実装

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, response::{Redirect, IntoResponse}, http::StatusCode};
 
 async fn hello_bird() -> &'static str {
     "Hello, bird!"
 }
 
+async fn youtube_location() -> impl IntoResponse {
+    (StatusCode::FOUND, Redirect::to("https://www.youtube.com/watch?v=9Gc4QTqslN4"))
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_bird));
+    let router = Router::new()
+        .route("/", get(hello_bird))
+        .route("/-1/seek", get(youtube_location));
 
     Ok(router.into())
 }
